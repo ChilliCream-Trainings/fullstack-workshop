ad228c95f918b764445e20a2b6a8d90efac9fb7f
Michael Staib <michael@chillicream.com>
2024-04-23 22:37:05 +0200

    Fixed filter

--- src/Chapter4/Lesson4/Complete/src/Catalog.API/Types/Filtering/ProductsFilterInputType.cs
+++ src/Chapter4/Lesson4/Complete/src/Catalog.API/Types/Filtering/ProductsFilterInputType.cs
@@ -7,6 +7,6 @@ public readonly record struct ProductsFilterInputType(
     public ProductFilter ToFilter() => new(BrandId?.In, TypeId?.In);
 }
 
-public readonly record struct ProductsBrandIdFilterInputType(int[]? In);
+public readonly record struct ProductsBrandIdFilterInputType([ID<Brand>] int[]? In);
 
-public readonly record struct ProductsTypeIdFilterInputType(int[]? In);
+public readonly record struct ProductsTypeIdFilterInputType([ID<ProductType>] int[]? In);
